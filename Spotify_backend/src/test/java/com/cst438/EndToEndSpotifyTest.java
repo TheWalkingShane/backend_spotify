b31f4dedad7c74349b13ecbb155ed81b69a7749a
Marcos <maarroyo@csumb.edu>
2023-12-04 17:55:47 -0800

    end to end selenium

--- Spotify_backend/src/test/java/com/cst438/EndToEndSpotifyTest.java
+++ Spotify_backend/src/test/java/com/cst438/EndToEndSpotifyTest.java
@@ -1,33 +1,37 @@
 package com.cst438;
 
 
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.TimeUnit;
+
+@SpringBootTest
 public class EndToEndSpotifyTest {
-/*
-    public static final String CHROME_DRIVER_FILE_LOCATION = "path/to/your/chromedriver";
-    public static final String URL = "http://localhost:3000"; // URL of your Spotify web app
-    public static final int SLEEP_DURATION = 1000; // 1 second.
+
+    public static final String CHROME_DRIVER_FILE_LOCATION = "path/to/chromedriver.exe";
+    public static final String URL = "http://localhost:3000"; // Adjust to your application's URL
 
     @Test
-    public void viewPlaylistTest() throws Exception {
+    public void userLoginAndProfileFetchTest() throws Exception {
         System.setProperty("webdriver.chrome.driver", CHROME_DRIVER_FILE_LOCATION);
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
         try {
             driver.get(URL);
-            Thread.sleep(SLEEP_DURATION);
+            Thread.sleep(1000); // Wait for the page to load
 
-            // Example: Click on the 'View Playlist' button/link
-            WebElement viewPlaylistButton = driver.findElement(By.id("viewPlaylist"));
-            assertNotNull(viewPlaylistButton, "View Playlist button not found");
-            viewPlaylistButton.click();
-            Thread.sleep(SLEEP_DURATION);
+            // Add steps to interact with your web application
+            // For example, login (if not using OAuth), navigate to user profile page, etc.
 
-            // Assert that the playlist is displayed
-            WebElement playlistElement = driver.findElement(By.id("playlist"));
-            assertNotNull(playlistElement, "Playlist not found");
+            // Check for certain elements to verify successful interaction
+            // Example: WebElement profileElement = driver.findElement(By.id("profileElementId"));
+            // assertNotNull(profileElement, "Profile element not found");
 
-            // Additional actions and assertions can be added here...
+            // More interactions and assertions as needed for your test scenario
 
         } catch (Exception ex) {
             throw ex;
@@ -35,6 +39,6 @@ public class EndToEndSpotifyTest {
             driver.quit();
         }
     }
-
-    // Additional test methods can be added here...*/
 }
+
+

ecb3f1231a5ddb218cab4db3843d8f20dc08e9ce
Marcos <maarroyo@csumb.edu>
2023-12-05 10:59:24 -0800

    updated userprofile

--- Spotify_backend/src/main/java/com/cst438/controller/UserProfile.java
+++ Spotify_backend/src/main/java/com/cst438/controller/UserProfile.java
@@ -1,12 +1,49 @@
 package com.cst438.controller;
 
 public class UserProfile {
-    // Define fields based on Spotify's user profile structure
     private String id;
     private String displayName;
+    private String email;
+    private String country;
 
-    public Object getId() {
-        return null;
+    // Constructor
+    public UserProfile() {
+        // Default constructor
     }
-    // Other fields and getters/setters
+
+    // Getters and Setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    // You can add more fields and corresponding getters/setters as needed
 }
+
--- Spotify_backend/src/test/java/com/cst438/JunitTestSpotify.java
+++ Spotify_backend/src/test/java/com/cst438/JunitTestSpotify.java
@@ -51,7 +51,12 @@ public class JunitTestSpotify {
         // Arrange
         String accessToken = "testToken";
         String url = "https://api.spotify.com/v1/me";
-        UserProfile mockResponse = new UserProfile(/* set properties */);
+        UserProfile mockResponse = new UserProfile();
+
+        mockResponse.setId("12345");
+        mockResponse.setDisplayName("Test User");
+
+
         when(restTemplate.exchange(eq(url), eq(HttpMethod.GET), any(HttpEntity.class), eq(UserProfile.class)))
                 .thenReturn(new ResponseEntity<>(mockResponse, HttpStatus.OK));
 
@@ -69,7 +74,7 @@ public class JunitTestSpotify {
     public void testGetUserProfile_SpotifyController() throws Exception {
         // Arrange
         String accessToken = "testToken";
-        UserProfile mockProfile = new UserProfile(/* set properties */);
+        UserProfile mockProfile = new UserProfile();
         when(spotifyService.getUserProfile(accessToken)).thenReturn(mockProfile);
 
         // Act & Assert
@@ -86,15 +91,21 @@ public class JunitTestSpotify {
         // Arrange
         String query = "testQuery";
         String accessToken = "testToken";
-        List<TrackDTO> mockSearchResults = /* Create a list of TrackDTO */;
-        when(searchService.searchTracks(query, accessToken)).thenReturn(mockSearchResults);
+        List<TrackDTO> mockSearchResults = List.of(
+                new TrackDTO("trackId1", "Track Name 1", "Artist Name 1", "Album Name 1"),
+                new TrackDTO("trackId2", "Track Name 2", "Artist Name 2", "Album Name 2")
+        );
+        doReturn(mockSearchResults).when(searchService).searchTracks(anyString(), anyString());
+
 
         // Act & Assert
         mockMvc.perform(get("/api/search").param("query", query).param("accessToken", accessToken))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$[0].name").value(mockSearchResults.get(0).name()));
-        // Add more expectations based on the TrackDTO structure
+                .andExpect(jsonPath("$[0].name").value(mockSearchResults.get(0).name()))
+                .andExpect(jsonPath("$[1].name").value(mockSearchResults.get(1).name()));
+        // Add more assertions as needed
     }
+
 
     // Additional tests for other functionalities can be added here
 }

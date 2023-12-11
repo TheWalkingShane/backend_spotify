a5d59937db8067fcfe364ada45513fd7e7836b92
Marcos <maarroyo@csumb.edu>
2023-12-10 22:59:33 -0800

    someupdates

--- Spotify_backend/src/main/java/com/cst438/controller/SpotifySearchResponse.java
+++ Spotify_backend/src/main/java/com/cst438/controller/SpotifySearchResponse.java
@@ -3,8 +3,21 @@ package com.cst438.controller;
 
 import java.util.List;
 
+import com.cst438.domain.TrackDTO;
+
 public class SpotifySearchResponse {
-    public List getTracks() {
-        return null;
+    private SpotifyTracks tracks;
+
+    public SpotifyTracks getTracks() {
+        return tracks;
+    }
+
+    public static class SpotifyTracks {
+        private List<TrackDTO> items;
+
+        public List<TrackDTO> getItems() {
+            return items;
+        }
     }
 }
+
